da5f3f36e0e7d4303da2241f887acf710ae0c57c
jacob <j.aymimir@gmail.com>
2022-05-25 12:27:35 +0200

    Commit Obtener usuario desde api RandomUser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import axios from "axios";
-import { Subscriber } from "rxjs";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = "app";
+  id = 0;
+  person: any;
   
+  async getRandomUserName() {
+    this.person = await this.callMethod(this.person);
+    this.ngOnDestroy();
+  }
+
+
+  async ngOnInit() {
+    this.person = await this.callMethod(this.person);
+    console.log(this.person);
+    this.id = window.setInterval(async () => {
+      this.person = await this.callMethod(this.person);
+    }, 10000);
+  }
+
+  ngOnDestroy() {
+    if (this.id) {
+      window.clearInterval(this.id);
+      this.id = window.setInterval(async () => {
+        this.person = await this.callMethod(this.person);
+      }, 10000);
+    }
+  }
+
+  async callMethod(person: Array<any>): Promise<any> {
+    var api = "https://randomuser.me/api/";
+
+    try {
+      const response = await axios.get(api);
+      person = response.data.results[0];
+      return person;
+    } catch (error) {
+      console.error(error);
+      return error;
+    }
+  }
 }
